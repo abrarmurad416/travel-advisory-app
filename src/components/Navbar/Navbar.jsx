264301593b962a089c977a97d1aa87ede98ab455
Abrar <abrarmurad00@gmail.com>
2023-05-18 15:48:38 -0400

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+
+export default function Navbar() {
+    return (
+        <div>
+            <h1>Header</h1>
+        </div>
+    );
+}
