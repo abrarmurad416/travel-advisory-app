ae182117a83fab9edb09181b9f7df6fa36307b15
Abrar <abrarmurad00@gmail.com>
2023-05-18 17:24:09 -0400

    changed navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,44 @@
 import React from "react";
 import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 
-export default function Navbar() {
+import useStyles from "./styles.js";
+
+const Navbar = ({ onPlaceChanged, onLoad }) => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <AppBar position="static" className={classes.appbar}>
+            <Toolbar className={classes.toolbar}>
+                <Typography variant="h5" className={classes.title}>
+                    Travel Advisor
+                </Typography>
+                <Box display="flex">
+                    <Typography variant="h6" className={classes.title}>
+                        Explore new places
+                    </Typography>
+                    {/*       <Autocomplete
+                        onLoad={onLoad}
+                        onPlaceChanged={onPlaceChanged}
+    > */}
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase
+                            placeholder="Find a location"
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                        />
+                    </div>
+                    {/* </Autocomplete> */}
+                </Box>
+            </Toolbar>
+        </AppBar>
     );
-}
+};
+
+export default Navbar;
